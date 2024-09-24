427ad8e948c1bdc7d45edc4c902668dea24c2e23
LuisGustavoPM <141171525+LuisGustavoPM@users.noreply.github.com>
2024-09-24 11:58:49 -0300

    d

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ function soma(num1,num2){
 }
 console.log(soma(2,3));
 
-console.log(multiplication(6,6));
+
 function areaQuadrado 
 (lado){
-    return lado**6;
+    return lado**2;
 }
 
 let areatrapezio = function(Base1,base2,altura){
